d4ebbeacb263620020ac13ae4cbf8e40a75cd90b
Monteiro <102252895+bjmontr@users.noreply.github.com>
2024-09-06 00:47:15 -0300

    modal

--- app.js
+++ app.js
@@ -4,7 +4,20 @@ const submit = document.getElementById("search");
 const btn = document.getElementById("btn-submit");
 const inputText = document.querySelector(".input-text");
 const characterContent = document.getElementsByClassName("cover-spider")[0];
+const main = document.getElementsByTagName("main")[0]; // Corrigido para selecionar o elemento main corretamente
 const footer = document.querySelector("footer");
+
+// Cria e adiciona o modal ao DOM uma vez
+const modal = document.createElement("section");
+modal.classList.add("modal-spider");
+modal.setAttribute("id", "modal-spider");
+modal.style.display = 'none'; // Inicialmente oculto
+main.appendChild(modal);
+
+const closeBtn = document.createElement('span');
+closeBtn.classList.add('close-btn');
+closeBtn.innerHTML = '&times;';
+modal.appendChild(closeBtn);
 
 // Função para atualizar o estado do botão
 function updateBtn() {
@@ -18,50 +31,93 @@ updateBtn();
 submit.addEventListener("input", updateBtn);
 
 // Evento de submit do formulário
-document
-  .querySelector(".content-input")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+document.querySelector(".content-input").addEventListener("submit", function (event) {
+  event.preventDefault();
 
-    const searchValue = inputText.value.toLowerCase();
-    console.log("Valor enviado:", searchValue);
+  const searchValue = inputText.value.toLowerCase();
+  console.log("Valor enviado:", searchValue);
 
-    const foundItems = spiderManVersions.filter((spider) =>
-      spider.name.toLowerCase().includes(searchValue)
-    );
+  const foundItems = spiderManVersions.filter((spider) =>
+    spider.name.toLowerCase().includes(searchValue)
+  );
 
-    const characterDivs = document.querySelectorAll(".character-content");
+  const characterDivs = document.querySelectorAll(".character-content");
 
-    if (foundItems.length > 0) {
-      characterDivs.forEach((div) => {
-        const spiderName = div.querySelector("h3").textContent.toLowerCase();
-        const isMatch = foundItems.some(
-          (spider) => spider.name.toLowerCase() === spiderName
-        );
-        div.style.display = isMatch ? "block" : "none";
-      });
-      footer.style.position = "relative";
-    } else {
-      console.log("Nenhum personagem encontrado");
-      characterDivs.forEach((div) => {
-        div.style.display = "none";
-      });
-      footer.style.position = "fixed";
-    }
+  if (foundItems.length > 0) {
+    characterDivs.forEach((div) => {
+      const spiderName = div.querySelector("h3").textContent.toLowerCase();
+      const isMatch = foundItems.some(
+        (spider) => spider.name.toLowerCase() === spiderName
+      );
+      div.style.display = isMatch ? "block" : "none";
+    });
+    footer.style.position = "relative";
+  } else {
+    console.log("Nenhum personagem encontrado");
+    characterDivs.forEach((div) => {
+      div.style.display = "none";
+    });
+    footer.style.position = "fixed";
+  }
 
-    // Limpa o campo de texto
-    inputText.value = "";
-    updateBtn(); // Atualiza o botão após o envio
-  });
+  // Limpa o campo de texto
+  inputText.value = "";
+  updateBtn(); // Atualiza o botão após o envio
+});
 
 // Cria os elementos de personagem e adiciona ao DOM
 spiderManVersions.forEach((version) => {
   const coverSpider = document.createElement("div");
   coverSpider.classList.add("character-content");
-  coverSpider.setAttribute("id", `${version.name}`);
+  coverSpider.setAttribute("id", version.name);
   coverSpider.innerHTML = `
-        <img src="${version.cover}" alt="${version.name}">
-        <h3>${version.name}</h3>
+    <img src="${version.cover}" alt="${version.name}">
+    <h3>${version.name}</h3>
+  `;
+  coverSpider.addEventListener('click', function() {
+    // Atualiza o conteúdo do modal
+    modal.innerHTML = `
+      <span class="close-btn">&times;</span>
+      <img src="${version.fullPortrait}" alt="${version.name}">
+      <div class="bio-content">
+        <h2>${version.name}</h2>
+        <div class="description">
+          <h3>INFORMAÇÕES</h3>
+          <div class="inf">
+            <p>Nome Completo</p>
+            <h4>${version.fullname}</h4>
+          </div>
+          <div class="inf">
+            <p>Terra</p>
+            <h4>${version.universe}</h4>
+          </div>
+          <div class="inf">
+            <h4>Descrição</h4>
+            <p>${version.description}</p>
+          </div>
+        </div>
+        <h3>PRIMEIRA APARIÇÃO</h3>
+        <img class="cover-description" src="${version.cover}" alt="${version.name}">
+      </div>
     `;
+
+    // Adiciona o botão de fechar ao modal
+    modal.appendChild(closeBtn);
+
+    // Exibe o modal
+    modal.style.display = 'flex';
+  });
+
   characterContent.appendChild(coverSpider);
+});
+
+// Configura o botão de fechar e a funcionalidade de clicar fora do modal
+closeBtn.addEventListener('click', function() {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
 });
--- data.js
+++ data.js
@@ -1,152 +1,155 @@
 const spiderManVersions = [
   {
-    name: "peter parker",
+    fullPortrait: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ea196117-0b64-49b7-b13f-79f43cf77e53/dcv26ch-537b2322-344a-44de-affe-43ea313d5de6.png/v1/fill/w_1053,h_759/into_the_spider_verse___spider_man__1____png_by_captain_kingsman16_dcv26ch-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9OTIzIiwicGF0aCI6IlwvZlwvZWExOTYxMTctMGI2NC00OWI3LWIxM2YtNzlmNDNjZjc3ZTUzXC9kY3YyNmNoLTUzN2IyMzIyLTM0NGEtNDRkZS1hZmZlLTQzZWEzMTNkNWRlNi5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.-ANvLSgC8ukjKeFTpVVhRcygSpiv0S05bWdQkKkIF-Y",
+    fullname: "Peter B. Parker",
+    name: "Spider-Man",
     firstAppearance: "Amazing Fantasy #15 (1962)",
     description:
       "O Homem-Aranha original, um jovem fotógrafo que ganha poderes aracnídeos após ser mordido por uma aranha radioativa.",
     cover: "https://m.media-amazon.com/images/I/51ylofh3QmL._SY580_.jpg",
+    universe: "Terra-199999"
   },
   {
+    fullPortrait: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5b8d2b12-21e8-4931-8a6d-fb9ecdd60383/dfzaix9-bb14e075-32d7-42f1-9116-2e11328a457e.png/v1/fill/w_1280,h_1566/across_the_spider_verse_miles_morales_png_by_metropolis_hero1125_dfzaix9-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTU2NiIsInBhdGgiOiJcL2ZcLzViOGQyYjEyLTIxZTgtNDkzMS04YTZkLWZiOWVjZGQ2MDM4M1wvZGZ6YWl4OS1iYjE0ZTA3NS0zMmQ3LTQyZjEtOTExNi0yZTExMzI4YTQ1N2UucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.KtgIn8OBEe-JwcQcZjfI_0PFItrn-zQI8RjRqVMr__g",
+    fullname: "Miles Gonzalo Morales",
     name: "Miles Morales",
     firstAppearance: "Ultimate Fallout #4 (2011)",
     description:
       "Um jovem afro-latino que assume o manto do Homem-Aranha no universo Ultimate após a morte de Peter Parker.",
     cover: "https://m.media-amazon.com/images/I/51L5qDky1kL._SY580_.jpg",
+    universe: "Terra-616"
   },
   {
+    fullPortrait: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ea196117-0b64-49b7-b13f-79f43cf77e53/dcv27e1-a2700589-88a3-4c7c-a51e-d689d0afb48a.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2VhMTk2MTE3LTBiNjQtNDliNy1iMTNmLTc5ZjQzY2Y3N2U1M1wvZGN2MjdlMS1hMjcwMDU4OS04OGEzLTRjN2MtYTUxZS1kNjg5ZDBhZmI0OGEucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.E-FaRe3uPLW7-BpqVF5Mc7Tk4-7I5IgrGNDzCr3oeUE",
+    fullname: "Gwendolyn Maxine Stacy",
     name: "Spider-Gwen",
     firstAppearance: "Edge of Spider-Verse #2 (2014)",
     description:
       "Gwen Stacy, no universo alternativo onde ela foi mordida por uma aranha radioativa, tornando-se a Spider-Woman.",
     cover: "https://m.media-amazon.com/images/I/51jP2NW8P5L.jpg",
+    universe: "Terra-65"
   },
   {
+    fullPortrait: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c7bced49-73a3-43c1-b3ce-fe419a596a72/dg1be31-dda43d85-f07b-4f54-8f53-a24f8c5de4f4.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2M3YmNlZDQ5LTczYTMtNDNjMS1iM2NlLWZlNDE5YTU5NmE3MlwvZGcxYmUzMS1kZGE0M2Q4NS1mMDdiLTRmNTQtOGY1My1hMjRmOGM1ZGU0ZjQucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.sLeNqjqYDcbln63FGWdLMN8gMOZqXdNl7BuVGZLlR7Q",
+    fullname: "Miguel O'Hara",
     name: "Spider-Man 2099",
     firstAppearance: "The Amazing Spider-Man #365 (1992)",
     description:
       "Um geneticista do ano 2099 que cria uma nova identidade de Homem-Aranha com poderes aprimorados.",
     cover: "https://m.media-amazon.com/images/I/517X2UHJ2hL.jpg",
+    universe: "TTerra-928"
   },
   {
+    fullPortrait: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f890a9ae-278c-482d-aa89-1e553b459310/dge55rz-a11ecd60-e3a2-4513-9cdc-ffb0401b22bd.png/v1/fill/w_720,h_701/scarlet_spider__across_the_spider_verse__png_by_jawlessarrowmagnet58_dge55rz-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzAxIiwicGF0aCI6IlwvZlwvZjg5MGE5YWUtMjc4Yy00ODJkLWFhODktMWU1NTNiNDU5MzEwXC9kZ2U1NXJ6LWExMWVjZDYwLWUzYTItNDUxMy05Y2RjLWZmYjA0MDFiMjJiZC5wbmciLCJ3aWR0aCI6Ijw9NzIwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.l_M_bg50LfaE7PVZM4ikpPP9s7m3svn-D8gSCqSeAtk",
+    fullname: "Ben Reilly",
     name: "Scarlet Spider",
     firstAppearance: "The Amazing Spider-Man #149 (1975)",
     description:
       "Um clone de Peter Parker que assume a identidade de Scarlet Spider antes de se tornar o Homem-Aranha Ben Reilly.",
     cover:
       "https://image.keycollectorcomics.com/media/amazing_spider-man_149.jpg",
+      universe: "Terra-616",
   },
   {
+    fullPortrait: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c1a9a6fa-bf6d-4ac3-b103-5ed371db466d/ddym783-2087c246-f5fd-4d70-9fdf-a322fa6b6cf3.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2MxYTlhNmZhLWJmNmQtNGFjMy1iMTAzLTVlZDM3MWRiNDY2ZFwvZGR5bTc4My0yMDg3YzI0Ni1mNWZkLTRkNzAtOWZkZi1hMzIyZmE2YjZjZjMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.Pi5bo8Ufri18XB9mFiRnP-8BjGdOtITxkIRHyRD1Lrc",
+    fullname: "Peter B. Parker",
     name: "Spider-Man Noir",
     firstAppearance: "Spider-Man: Noir #1 (2009)",
     description:
       "Uma versão do Homem-Aranha ambientada na década de 1930, com uma estética noir e métodos mais sombrios.",
     cover: "https://m.media-amazon.com/images/I/51LQIWq35tL.jpg",
+    universe: "Terra-90214",
   },
   {
+    fullPortrait: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ea196117-0b64-49b7-b13f-79f43cf77e53/dcv863p-98cb2fc0-ae31-4cbe-8bce-54d3e77214f5.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2VhMTk2MTE3LTBiNjQtNDliNy1iMTNmLTc5ZjQzY2Y3N2U1M1wvZGN2ODYzcC05OGNiMmZjMC1hZTMxLTRjYmUtOGJjZS01NGQzZTc3MjE0ZjUucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.Kagl80C-2DIs-X3K8D1eAdEYbgaPBpj9KspPD1R2vGc",
+    fullname: "Peni Parker",
     name: "SP//dr",
     firstAppearance: "Edge of Spider-Verse #5 (2014)",
     description:
       "Uma jovem pilotando um mecha bioprostético chamado SP//dr, em um universo futurista.",
     cover: "https://m.media-amazon.com/images/I/51Bn81LtJqL.jpg",
+    universe: "Earth-14512"
   },
   {
+    fullPortrait: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c1a9a6fa-bf6d-4ac3-b103-5ed371db466d/ddzcioe-2dc8a953-abe8-4789-a622-c978f3eeec9a.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2MxYTlhNmZhLWJmNmQtNGFjMy1iMTAzLTVlZDM3MWRiNDY2ZFwvZGR6Y2lvZS0yZGM4YTk1My1hYmU4LTQ3ODktYTYyMi1jOTc4ZjNlZWVjOWEucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BECa9j7GbS8SOiMowaunQCuohDXpUZzuKYpLsngWu_o",
+    fullname: "Peter Porker",
     name: "Spider-Ham",
     firstAppearance:
       "Marvel Tails Starring Peter Porker, the Spectacular Spider-Ham #1 (1983)",
     description:
       "Uma versão cômica do Homem-Aranha como um porco antropomórfico.",
     cover: "https://m.media-amazon.com/images/I/61mmcj6YScL._SY780_.jpg",
+    universe: "Terra-8311"
   },
   {
+    fullPortrait: "/src/img/2211.png",
+    fullname: "Max Borne",
     name: "Spider-Man 2211",
     firstAppearance: "The Superior Spider-Man #1 (2013)",
     description:
       "Uma versão do Homem-Aranha do futuro distante, com habilidades e tecnologia avançadas.",
     cover: "https://m.media-amazon.com/images/I/61BcOUDMHiL.jpg",
+    universe: "Terra 9500"
   },
   {
-    name: "Cindy Moon",
+    fullPortrait: "/src/img/Silk.webp",
+    fullname: "Cindy Moon",
+    name: "Silk",
     firstAppearance: "The Amazing Spider-Man vol. 3 #1 (2014)",
     description:
       "Uma colega de Peter Parker que ganha poderes semelhantes aos do Homem-Aranha após ser mordida pela mesma aranha.",
     cover: "https://m.media-amazon.com/images/I/616zBw1q1IL.jpg",
+    universe: "Terra-616"
   },
   {
+    fullPortrait: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d9d54c40-e2e2-4f98-9b52-535211193e7d/dfuysdm-e68e9d0a-e79a-48a2-ac99-9d4dc35ac745.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Q5ZDU0YzQwLWUyZTItNGY5OC05YjUyLTUzNTIxMTE5M2U3ZFwvZGZ1eXNkbS1lNjhlOWQwYS1lNzlhLTQ4YTItYWM5OS05ZDRkYzM1YWM3NDUucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BX36VRA4ED5XR3IL9HY9JZ2EUAL6CC_P-DVf_41Mblw",
+    fullname: "William Braddock",
     name: "Spider-UK",
     firstAppearance: "Spider-Men II #1 (2017)",
     description:
       "Um membro da Young Masters do Reino Unido e parte do multiverso de Homens-Aranha.",
     cover: "https://m.media-amazon.com/images/I/51pACYUUoyL.jpg",
+    universe: "Terra-833"
   },
   {
+    fullPortrait: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5b8d2b12-21e8-4931-8a6d-fb9ecdd60383/dfzaixs-e72547a5-442b-4d1f-9f9d-0b5cd75afe26.png/v1/fill/w_1280,h_1283/across_the_spider_verse_pavitar_prabhakar_png_by_metropolis_hero1125_dfzaixs-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTI4MyIsInBhdGgiOiJcL2ZcLzViOGQyYjEyLTIxZTgtNDkzMS04YTZkLWZiOWVjZGQ2MDM4M1wvZGZ6YWl4cy1lNzI1NDdhNS00NDJiLTRkMWYtOWY5ZC0wYjVjZDc1YWZlMjYucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.Ba7mYYOssm_C63c-WshGBNoS71Z4QFLfChrv4nhCoWU",
+    fullname: "Pavitr Prabhakar ",
     name: "Spider-Man India",
     firstAppearance: "Spider-Man: India #1 (2004)",
     description:
       "Uma versão do Homem-Aranha ambientada na Índia, com elementos culturais locais incorporados.",
     cover: "https://m.media-amazon.com/images/I/51XKw7YRlfL.jpg",
+    universe: "Terra-50101"
   },
   {
-    name: "Spider-Man 2211",
-    firstAppearance: "Spider-Man 2099 Meets Spider-Man #1 (1995)",
-    description:
-      "Um agente da polícia temporal que utiliza um traje tecnologicamente avançado com múltiplos braços mecânicos. Ele protege as linhas do tempo e luta contra ameaças interdimensionais.",
-    cover: "https://m.media-amazon.com/images/I/61CYb7vyI3L.jpg",
-  },
-  {
-    name: "Spider-Man Unlimited",
-    firstAppearance: "Spider-Man Unlimited #1 (1993)",
-    description:
-      "Eddie Brock, em uma versão alternativa, se torna o Homem-Aranha em uma realidade com um traje especial que possui habilidades extras, lutando contra ameaças interdimensionais.",
-    cover: "https://m.media-amazon.com/images/I/61TCrx2LNkL.jpg",
-  },
-  {
-    name: "Scarlet Spider",
-    firstAppearance: "The Amazing Spider-Man #700 (2012)",
-    description:
-      "Um clone defeituoso de Peter Parker que assume a identidade de Scarlet Spider, buscando redenção.",
-    cover: "https://m.media-amazon.com/images/I/61kS8AK2iYL._SY580_.jpg",
-  },
-  {
-    name: "Spider-Man 1602",
-    firstAppearance: "Spider-Man 1602 #1 (2004)",
-    description:
-      "Peter Parquagh é o Homem-Aranha em um universo ambientado no ano de 1602, com uma abordagem renascentista e influências históricas na sua origem e poderes.",
-    cover: "https://m.media-amazon.com/images/I/61hWn60vZoL.jpg",
-  },
-  {
+    fullPortrait: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1ebd221d-3581-4775-8f4c-086c377bc4c3/dfym10h-96fe2414-c390-4612-80d1-3b075e35d167.png/v1/fill/w_1280,h_854/across_the_spider_verse_jessica_drew_png_by_docbuffflash82_dfym10h-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODU0IiwicGF0aCI6IlwvZlwvMWViZDIyMWQtMzU4MS00Nzc1LThmNGMtMDg2YzM3N2JjNGMzXC9kZnltMTBoLTk2ZmUyNDE0LWMzOTAtNDYxMi04MGQxLTNiMDc1ZTM1ZDE2Ny5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.dZ3QuBejpqxGLQ0ZPoaN4UeDg3z6LbsyfVMHXL_zVFk",
+    fullname: "Jessica Drew",
     name: "Spider-Woman",
     firstAppearance: "Marvel Spotlight #32 (1977)",
     description:
       "Embora não seja uma versão direta do Homem-Aranha, Jessica Drew compartilha algumas origens e poderes semelhantes.",
     cover: "https://m.media-amazon.com/images/I/61YXhRJmHtL.jpg",
+    universe: "Terra-616"
   },
   {
-    name: "Spider-Armor",
-    firstAppearance: "The Amazing Spider-Man #1 (2015)",
-    description:
-      "Várias versões do Homem-Aranha que utilizam trajes tecnológicos avançados para aprimorar suas habilidades.",
-    cover: "https://m.media-amazon.com/images/I/61OT83sTzTL._SY580_.jpg",
-  },
-  {
-    name: "Spider-Man (Jakob P.)",
-    firstAppearance: "Spider-Man: Reign #1 (2006)",
-    description:
-      "Jakob P. é um Homem-Aranha em uma realidade distópica e mais sombria, onde ele enfrenta um regime opressor em um futuro onde os heróis estão em baixa.",
-    cover: "https://m.media-amazon.com/images/I/51LqGOy8ksL._SY780_.jpg",
-  },
-  {
+    fullPortrait: "/src/img/Spider-Ma.png",
+    fullname: "Maybelle Parker",
     name: "Spider-Ma'am",
     firstAppearance: "What If? #105 (1978)",
     description:
       "Uma história alternativa onde a tia de Peter Parker, May Parker, torna-se o Homem-Aranha após a morte de Peter.",
     cover: "https://m.media-amazon.com/images/I/51gzsMFCIKL.jpg",
+    universe: "Terra-3123"
   },
   {
-    name: "Spider-Man (Dinosaur)",
+    fullPortrait: "/src/img/rex.png",
+    fullname: "Pter Ptarker",
+    name: "Spider-Rex",
     firstAppearance: "Spider-Verse #1 (2014)",
     description:
       "Uma versão alternativa do Homem-Aranha em que Peter Parker é um dinossauro que possui habilidades aracnídeas, combinando poderes de aranha com características de dinossauro.",
     cover: "https://m.media-amazon.com/images/I/61S2MLuH4JL.jpg",
+    universe: "Terra-66"
   },
 ];
 
